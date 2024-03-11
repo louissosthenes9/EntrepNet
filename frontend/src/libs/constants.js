e24ce86c36c0fdc8b1de910f817cb4057783d5c3
loussosthenes9 <louissosthenes9@gmail.com>
2024-03-12 01:16:54 +0300

    images and urls

---
+++ frontend/src/libs/constants.js
@@ -0,0 +1,36 @@
+export const images = [
+    {
+        name:"image1",
+        src:"/genge.svg",
+        pos:"col-start-1"     
+    },
+    {
+        name:"placeholder1",
+           
+    },
+
+    {
+        name:"image2",
+        src:"/mfugaji.svg",
+        pos:"col-start-2"         
+    },
+    {
+        name:"placeholder2",
+           
+    },
+    {
+        name:"image3",
+        src:"/mkulima.svg",  
+        pos:"col-start-1"       
+    },
+
+    {
+        name:"placeholder1",
+           
+    },
+    {
+        name:"image4",
+        src:"/cherehani.svg",
+        pos:"col-start-2" ,     
+    },
+]
