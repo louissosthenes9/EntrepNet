1ed679bd27f9fee20ac27f0719f684ce0177c5c1
loussosthenes9 <louissosthenes9@gmail.com>
2024-03-12 01:13:50 +0300

    completed hero section

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ({text}) {
+  return (
+    <button className='bg-[#FCA311] rounded-lg  text-lg px-4 py-1  font-light w-32'>
+             {text}
+    </button>
+
+  )
+}
---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Button from './Button'
+
+export default function Hero() {
+  return (
+    <div className="flex flex-col gap-12">
+    <div className="text-white text-3xl font-extrabold ">
+        Get connected to your <br />
+        fellow entrepreneurs
+     </div>
+    <div className="text-white text-lg">
+       EnterpeneurConnect connects you <br />
+       with like minded entrepreneurs for sharing <br />
+       experiences and opportunities as well as <br />
+       getting mentorship while expanding your <br />
+       market and experience
+    </div>
+   <Button text="Continue" />
+</div>
+  )
+}
