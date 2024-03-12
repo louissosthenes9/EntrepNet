3a68ed0bae10165aee27907c17dc19bc2b9364ce
loussosthenes9 <louissosthenes9@gmail.com>
2024-03-12 17:11:28 +0300

    trying a new layout approach

--- frontend/src/components/ImageGrid.jsx
+++ frontend/src/components/ImageGrid.jsx
@@ -1,25 +1,34 @@
 import React from "react";
-import { images } from "../libs/constants";
+import { imageSet1, imageSet2 } from "../libs/constants";
 import Image from "next/image";
 export default function ImageGrid() {
   return (
-    <div className="grid grid-cols-2">
-      
-        {images.map((imageObj,index) => (
-         <div 
-           
-           key={imageObj.name}>
-           <Image
-              src={imageObj.src}
-              width={150}
-              height={150}
-              alt={imageObj.name}
-            />
-         </div>
-           
-         
+    <>
+      <div className="grid grid-cols-2">
+        {imageSet1.map((imageObj) => (
+          <Image
+            key={imageObj.name}
+            className= {`${imageObj.class}`}
+            src={imageObj.src}
+            width={150}
+            height={150}
+            alt={imageObj.name}
+          />
         ))}
-    
-    </div>
+      </div>
+      <div className="grid grid-cols-2">
+        {imageSet2.map((imageObj) => (
+          <Image
+            key={imageObj.name}
+            className= {`${imageObj.class}`}
+            src={imageObj.src}
+            width={150}
+            height={150}
+            alt={imageObj.name}
+          />
+        ))}
+      </div>
+   
+    </>
   );
 }
