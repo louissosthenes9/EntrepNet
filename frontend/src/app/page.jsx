4645a0677c229a3b74c5cf2cf88d1a6a054b8e72
loussosthenes9 <louissosthenes9@gmail.com>
2024-03-12 22:42:41 +0300

    authentication pages

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,10 +1,10 @@
-import Button from "@/components/Button";
+
 import ImageGrid from "@/components/ImageGrid";
 import MaxWidthWrapper from "@/components/MaxwidthWrapper";
 import React from "react";
-import { images } from "../libs/constants";
-import Image from "next/image";
+
 import Hero from "@/components/Hero";
+
 
 export default function () {
   return (
