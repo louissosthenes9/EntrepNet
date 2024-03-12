3304e7725c298d2d7cac7f6737c2b535677120ba
loussosthenes9 <louissosthenes9@gmail.com>
2024-03-12 22:39:30 +0300

    fix image classes

--- frontend/src/libs/constants.js
+++ frontend/src/libs/constants.js
@@ -1,22 +1,24 @@
 export const imageSet1= [
+
     {
         name:"image1",
         src:"/genge.svg",
-        class:""     
+        class:"col-start-2"     
     },
-   
-
+    
     {
         name:"image2",
         src:"/mfugaji.svg",
-        class:""         
+        class:"col-start-1"         
     },
+
+    
 ]
 export const imageSet2 = [
     {
         name:"image3",
         src:"/mkulima.svg",  
-        class:""       
+        class:"col-start-1"       
     },
 
    
