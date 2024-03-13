a1c0ea1ba8d250e38b7fe22b6fd873bc4d9f607b
loussosthenes9 <louissosthenes9@gmail.com>
2024-03-13 14:38:44 +0300

    libs changed

---
+++ frontend/src/lib/constants.js
@@ -0,0 +1,28 @@
+export const imageSet1= [
+    {
+        name:"image1",
+        src:"/genge.svg",
+        class:""     
+    },
+   
+
+    {
+        name:"image2",
+        src:"/mfugaji.svg",
+        class:""         
+    },
+]
+export const imageSet2 = [
+    {
+        name:"image3",
+        src:"/mkulima.svg",  
+        class:""       
+    },
+
+   
+    {
+        name:"image4",
+        src:"/cherehani.svg",
+        class:"" ,     
+    },
+]
