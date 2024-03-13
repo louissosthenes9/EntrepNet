9d54256458fa5d371f91e265ab892bd309aa525c
loussosthenes9 <louissosthenes9@gmail.com>
2024-03-13 17:38:41 +0300

    signup form

--- frontend/src/components/SignUpForm.jsx
+++ frontend/src/components/SignUpForm.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { Input } from "@/components/ui/input";
+import Button from "./Button";
+import FormHeader from "./FormHeader"
 
 export default function SignUpForm() {
-    
+  const formik = useFormik({
+    initialValues: {
+      password: "",
+      email: "",
+    },
+    validationSchema: Yup.object({
+      password: Yup.string()
+        .min(8, "password should have atleast 8 characters")
+        .required("Required"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+    }),
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
-    <div>SignUpForm</div>
-  )
+    <>
+      <div>
+        <form
+          onSubmit={formik.handleSubmit}
+          className="flex flex-col gap-y-12 items-center justify-center"
+        >
+        <FormHeader text={"Sign in to continue"}/>
+          <Input
+            id="email"
+            name="email"
+            type="email"
+            placeholder="you@email.com"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <div>{formik.errors.email}</div>
+          ) : null}
+          <Input
+            id="password"
+            name="password"
+            type="text"
+            placeholder="password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <div>{formik.errors.password}</div>
+          ) : null}
+
+          <Button text={"sign in"} />
+        </form>
+      </div>
+    </>
+  );
 }
