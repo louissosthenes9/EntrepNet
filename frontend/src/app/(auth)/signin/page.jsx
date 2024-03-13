ef3d2249c260fc6f82a2a2b804981e4e13591a4a
loussosthenes9 <louissosthenes9@gmail.com>
2024-03-13 17:41:45 +0300

    new changes

--- frontend/src/app/(auth)/signin/page.jsx
+++ frontend/src/app/(auth)/signin/page.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import MaxWidthWrapper from '@/components/MaxwidthWrapper';
+import SignUpForm from '@/components/SignUpForm'
 import ImageGrid from '@/components/ImageGrid'
 export default function page() {
   return (
     <> 
     <MaxWidthWrapper>
-      <div className="flex gap-x-24">
-       <div>form</div>
+      <div className="flex gap-x-48">
+       <SignUpForm />
         <ImageGrid />
       </div>
     </MaxWidthWrapper>
