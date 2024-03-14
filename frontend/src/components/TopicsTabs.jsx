97874d8de7a7996a7ac9ab787c22a0a13da752aa
loussosthenes9 <louissosthenes9@gmail.com>
2024-03-14 18:27:11 +0300

    topics logic

---
+++ frontend/src/components/TopicsTabs.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+export default function TopicsTabs() {
+  const choices = [
+    "Furniture",
+    "Agriculture",
+    "Business",
+    "Brokers",
+    "Salon",
+    "Transport",
+    "Baking",
+    "Fashion",
+  ];
+
+  // State to track selected topics
+  const [selectedTopics, setSelectedTopics] = useState([]);
+
+  // Function to handle button click
+  const handleButtonClick = (choice) => {
+    // Disable the button once selected
+    const updatedChoices = choices.filter((item) => item !== choice);
+    setSelectedTopics([...selectedTopics, choice]);
+  };
+
+  const getSelectedTopics = () => {
+    return selectedTopics;
+  };
+
+  return (
+    <>
+      <div className="grid grid-cols-1 gap-y-4">
+        <div className="text-lg font-bold text-white">
+          Choose topics of your interest
+        </div>
+        <div className="grid grid-cols-4 gap-2">
+          {choices.map((choice, index) => (
+            <button
+              key={index}
+              className={`bg-[#D9D9D9] rounded-full hover:bg-orange-400 hover:text-white text-lg px-4 py-1 font-medium w-32 ${
+                selectedTopics.includes(choice) ? "opacity-50 cursor-not-allowed" : ""
+              }`}
+              onClick={() => handleButtonClick(choice)}
+              disabled={selectedTopics.includes(choice)}
+            >
+              {choice}
+            </button>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
