db940b5f5a4801cd9130ee692db81d3a44a62dce
louissosthenes9 <louissosthenes9@gmail.com>
2024-03-28 01:14:26 +0300

    new layout

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,7 +1,5 @@
 
 import "./globals.css";
-import  Header from "../components/Header";
-
 export const metadata = {
   title: "EntrepNet",
   description: "A platform for enterpreneurs",
@@ -11,7 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="bg-gradient-to-br from-[#183678]  to-[#183678]">
-        <Header />
+     
         {children}
        </body>
     </html>
