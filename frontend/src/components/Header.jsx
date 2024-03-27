5e5d9eddce0c59f67bbaf0f7ff025875a58183c5
louissosthenes9 <louissosthenes9@gmail.com>
2024-03-28 02:11:25 +0300

    added props for size

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Button from "./Button";
 
-export default function Header() {
+export default function Header({fontSize}) {
   return (
   
       <div className="flex gap-x-5 items-center">
       <button className='bg-[#FCA311] font-bold h-10 w-10 px'>
             logo
        </button>
-      <div className="font-bold text-4xl">Entrep<span className="text-orange-400 font-medium">Net</span></div>
+      <div className={`font-bold ${fontSize}`}>Entrep<span className="text-orange-400 font-medium">Net</span></div>
 
     </div>
 
