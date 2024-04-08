13014b9243c64504c3c8166c8287051562fb0f05
louissosthenes9 <louissosthenes9@gmail.com>
2024-04-08 18:10:42 +0300

    side bar completed

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import Header from './Header'
 import { Separator } from './ui/separator'
+import { BsBookmarks } from "react-icons/bs";
+import { BiHelpCircle } from "react-icons/bi";
+import { HiOutlineSpeakerphone } from "react-icons/hi";
 import { Settings,LogOut,Pencil, Mails, CircleHelp} from 'lucide-react';
 
 export default function Sidebar() {
   return (
-    <div className='flex flex-col h-full gap-y-24 w-48 mx-12 mt-10'>
-        <Header fontSize={"text-lg"} />
-        <div><Separator orientation='horizontal' className="w-full" /></div>  
-        <Settings />
-        <LogOut />
-        <Pencil />
+    <div className='flex flex-col gap-y-12 w-48 mx-0 mt-10 bg-slate-600'>
+        <Header fontSize={"text-lg"} className =" justify-center items-center"/>
+        <div><Separator orientation='horizontal' className="w-full" /></div> 
+        <div className='flex flex-col gap-y-8 pl-6 '>
+             <div className='text-white flex gap-x-4'><Mails  /><div>Inbox</div></div>
+             <div className='text-white flex gap-x-4'><BsBookmarks className='text-lg font-bold'/> <div>Bookmarks</div></div>
+             <div className='text-white flex gap-x-4'><Pencil /><div>create a post</div></div>
+             <div className='text-white flex gap-x-4'><Settings /> <div> settings</div></div>
+             <div className="text-white flex gap-x-4"><HiOutlineSpeakerphone className='text-2xl font-extrabold'/><div>Bank offers</div></div>  
+        </div> 
+        <div><Separator orientation='horizontal' className="w-full" /></div> 
+        <div className="flex flex-col gap-y-8 pl-6">
+             <div className="text-white flex gap-x-4"><BiHelpCircle  className='text-2xl font-extrabold'/><div>Help</div></div>
+             <div className='text-white flex gap-x-4'> <LogOut /><div>logout</div></div>        
+        </div>
+       
     </div>
   )
 }
