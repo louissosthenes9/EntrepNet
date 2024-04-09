e39835f214582f773e3f642c585da848716425a5
louissosthenes9 <louissosthenes9@gmail.com>
2024-04-10 00:59:36 +0300

    starting layout

--- frontend/src/app/home/page.jsx
+++ frontend/src/app/home/page.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Sidebar from '@/components/Sidebar'
+import FeedSection from '@/components/FeedSection'
+import RightBar from '@/components/RightBar'
 
 export default function page() {
   return (
-    <div className='h-full'>
+    <div className='h-full grid grid-cols-3'>
         <Sidebar />
+        <FeedSection/>
+        <RightBar />
     </div>
   )
 }
