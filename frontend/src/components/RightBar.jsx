f389276ce0b5ad09029542c57245a6fc0ecca6d8
louissosthenes9 <louissosthenes9@gmail.com>
2024-04-11 12:04:55 +0300

    solved the client bug

--- frontend/src/components/RightBar.jsx
+++ frontend/src/components/RightBar.jsx
@@ -1,7 +1,15 @@
+"use client"
+import { CircleAlertIcon } from 'lucide-react'
 import React from 'react'
+import Notification from "./Notification"
 
 export default function RightBar() {
   return (
-    <div>rih</div>
+    <div  className="mt-8 mr-0">
+         <div className="flex gap-x-8"><Notification/><CircleAlertIcon/></div>
+         <div>
+             
+         </div>
+    </div>
   )
 }
