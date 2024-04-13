beb9ce74b1119bced4e7baf5ec505425b2c6aebb
louissosthenes9 <louissosthenes9@gmail.com>
2024-04-13 14:10:23 +0300

    finished side bar

--- frontend/src/components/RightBar.jsx
+++ frontend/src/components/RightBar.jsx
@@ -1,17 +1,19 @@
 "use client"
-import Profile from './Profile'
-import React from 'react'
-import Notification from "./Notification"
+import React from "react";
+import Profile from "./Profile";
+import Notification from "./Notification";
+import FollowingList from "./FollowingsList"
 export default function RightBar() {
   return (
-    <div  className="mt-8 ">
-         <div className="flex gap-x-8">
-          <Notification/>
-          <Profile />
-        </div>
-         <div>
-             
-         </div>
+    <div className="mt-8 ">
+      <div className="flex gap-x-8">
+        <Notification />
+        <Profile />
+      </div>
+      <div>
+          <div className="text-white">Suggested people</div>
+           <FollowingList />
+      </div>
     </div>
-  )
+  );
 }
