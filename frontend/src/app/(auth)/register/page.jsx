c0367a9a1d68db8d8522e9a56e1adbf8fe2c8562
louissosthenes9 <louissosthenes9@gmail.com>
2024-04-17 10:47:47 +0300

    added a multistep form

--- frontend/src/app/(auth)/register/page.jsx
+++ frontend/src/app/(auth)/register/page.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import MaxWidthWrapper from '@/components/MaxwidthWrapper';
 import RegisterForm from '@/components/RegisterForm'
 import ImageGrid from '@/components/ImageGrid'
+import FormWrapper from '../../../components/FormWrapper';
 export default function page() {
   return (
     <> 
     <MaxWidthWrapper>
       <div className="flex gap-x-48 ">
-        <RegisterForm/>
+        <FormWrapper/>
         <ImageGrid  />
       </div>
     </MaxWidthWrapper>
