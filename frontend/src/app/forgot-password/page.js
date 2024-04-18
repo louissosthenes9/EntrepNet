af9a6939d3dd8e5ae25bc43203e8ea0f09e18365
louissosthenes9 <louissosthenes9@gmail.com>
2024-04-18 23:52:07 +0300

    initial setup

---
+++ frontend/src/app/forgot-password/page.js
@@ -0,0 +1,52 @@
+"use client";
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { Input } from "@/components/ui/input";
+import Button from "@/components/Button";
+import FormHeader from "@/components/FormHeader"
+
+
+export default function page() {
+  
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Invalid email address").required("required"),
+    }),
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <>
+      <div>
+        <form
+          onSubmit={formik.handleSubmit}
+          className="flex flex-col gap-y-12 items-center justify-center"
+        >
+        <FormHeader className="mt-12" text={"Type your email"}/>
+        <div>
+        <Input
+            id="email"
+            name="email"
+            type="email"
+            placeholder="Email"
+            className="w-64"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <span className="text-sm font-light text-red-500">{formik.errors.email}</span>
+          ) : null}
+        </div>
+          <Button text={"Send email"} />
+          <div className="text-white">New here?<span className="text-[cyan]"><a href="/register">Register here</a></span></div>
+        </form>
+      </div>
+    </>
+  );
+}
