87eeb0fd0a0c7810d2202fe26ffefc195c66c742
louissosthenes9 <louissosthenes9@gmail.com>
2024-05-15 01:34:39 +0300

    reconverted

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function ({text}) {
   return (
-    <button type= "submit" className='bg-[#FCA311] rounded-lg  text-lg px-4 py-1  font-medium w-32'>
-             {text}
+    <button type='submit' className='bg-[#FCA311] rounded-lg  text-lg px-4 py-1  font-medium w-32'>
+            {text}
     </button>
 
   )
