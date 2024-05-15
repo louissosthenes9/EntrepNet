878db4e163d428736a14b9fcaabf3baa97607990
louissosthenes9 <louissosthenes9@gmail.com>
2024-05-16 00:16:05 +0300

    hardcorded  posts

---
+++ frontend/src/lib/Posts.js
@@ -0,0 +1,24 @@
+export const PostsList = [
+    {
+        name:"Jane John",
+        job:"Cake baker",
+        role:"Enterpreneur",
+        likes:20,
+        comments:10,
+        profilePic:"/erose.svg",
+        postContent:"Best ways for keepPROing healthy milking cows must always involve vacination.",
+        src:"/cow.svg"
+    },
+    {   
+        name:"John Baker",
+        profilePic:"/mdada1.svg",
+        job:"Cake baker",
+        role:"Mentor",
+        postContent:"New ways for baking!",
+        likes:34,
+        comments:234,
+        src:"/cake1.svg"
+    },
+
+
+]
