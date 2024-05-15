2ed7319c0cad0b196d08d72441e45760421c368c
louissosthenes9 <louissosthenes9@gmail.com>
2024-05-16 00:15:24 +0300

    added hardcorded tweets

--- frontend/src/components/FeedSection.jsx
+++ frontend/src/components/FeedSection.jsx
@@ -1,9 +1,61 @@
 import React from 'react'
-
+import Image from 'next/image'
+import { PostsList } from '../lib/Posts'
 export default function FeedSection() {
   return (
-    <div>
-        hhhh
-    </div>
+    <>
+      <div className='flex flex-col gap-y-4 mt-12'>
+        {
+          PostsList.map((following, index) => (
+
+            // Display the list of hardCorded tweets
+            <div>
+
+            {/* This is for the user and the profile */}
+              <div
+                className="flex gap-x-1 flex-shrink items-center"
+                key={following.name}
+              >
+                <div>
+                  <Image
+                    src={following.profilePic}
+                    width={40}
+                    height={40}
+                    alt="profile"
+                  />
+                </div>
+
+                <div className="grid grid-cols-1">
+                  <div className="text-white text-sm text-nowrap">{following.name}</div>
+                  <div className="flex space-x-1 items-center text-white">
+                    <div className="text-xs">{following.role}</div>
+                    <div className="text-white">|</div>
+                    <div className="text-white text-xs text-nowrap">{following.job}</div>
+                  </div>
+                </div>
+              </div>
+            
+
+            {/* This is the div for tweets */}
+               <div className='text-white font-thin text-sm my-4'>{following.postContent}</div>
+
+            {/* This is the div for images */}
+              <div>
+                <Image 
+                src={following.src}
+                width={400}
+                height={250}
+                alt='Post'
+                />
+              </div>
+
+            </div>
+
+
+          ))
+        }
+      </div>
+
+    </>
   )
 }
